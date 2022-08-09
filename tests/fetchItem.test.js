3230953273886e4fd8e223a724fa89350ba17907
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 18:20:46 -0300

    Testar se retona o mesmo item

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -22,7 +22,7 @@ describe('2 - Teste a função fetchItem', () => {
 
   test('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
     expect.assertions(1);
-    const objeto = await fetchItem('computador');
+    const objeto = await fetchItem('MLB1615760527');
     expect(objeto).toEqual(item);
   })
 });
