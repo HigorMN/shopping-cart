f6808f0258c52865dcffb7d961bf8952343051b5
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 16:01:31 -0300

    refatorando o link e pegando o results

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,12 @@
-const fetchProducts = async () => {
-  const endPoint = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+const fetchEndPoint = (product) => fetch('https://api.mercadolibre.com/sites/MLB/search?q=${product}');
+
+const fetchProducts = async (product) => {
+  const endPoint = await fetchEndPoint(product)
+  const result = await endPoint.json();
+  console.log(result.results);
 };
+
+fetchProducts('computador')
 
 if (typeof module !== 'undefined') {
   module.exports = {
