ad6094dbb4167e9c94b2ee9f5b4a5f3176e8ff43
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 18:17:57 -0300

    Testar se Retorna o fetch

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,7 +3,14 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  test('Teste se fetchProducts é uma função;', () => {
+  test('Teste se fetchItem é uma função;', () => {
     expect(typeof fetchItem).toBe('function');
   })
+
+  test('Execute a função fetchItem com o argumento \'MLB1615760527\' e teste se fetch foi chamada;', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+
 });
