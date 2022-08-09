7e5b892943c8acd42aa53b1bd541c6ede21e6286
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 18:20:34 -0300

    Testar se retona o mesmo item

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -15,8 +15,14 @@ describe('2 - Teste a função fetchItem', () => {
 
   test('Teste se, ao chamar a função fetchItem com o argumento do item \'MLB1615760527\', a função fetch utiliza o endpoint', async () => {
     expect.assertions(1);
-    await fetchProducts('MLB1615760527');
+    await fetchItem('MLB1615760527');
     const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527'
     expect(fetch).toBeCalledWith(endpoint);
+  })
+
+  test('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
+    expect.assertions(1);
+    const objeto = await fetchItem('computador');
+    expect(objeto).toEqual(item);
   })
 });
