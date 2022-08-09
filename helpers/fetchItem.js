e4adff87cc6335298135e0b3d43393060b96fcd2
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 18:35:59 -0300

    fetchItem criado

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,15 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchTake = (product) => fetch(
+  `https://api.mercadolibre.com/items/${product}`,
+);
+
+const fetchItem = async (product) => {
+  try {
+    const endPoint = await fetchTake(product);
+    const dataAPI = await endPoint.json();
+    return await dataAPI;
+  } catch (error) {
+    return 'You must provide an url';
+  }
 };
 
 if (typeof module !== 'undefined') {
