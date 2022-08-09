e81647169393d65d72cf7ebfca5a3d3cbbb5dfcb
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 15:11:44 -0300

    testar se a fetch foi chamada

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,4 +6,10 @@ describe('1 - Teste a função fetchProducts', () => {
   test('Teste se fetchProducts é uma função;', () => {
     expect(typeof fetchProducts).toBe('function')
   })
+
+  test('Execute a função fetchProducts com o argumento \'computador\' e teste se fetch foi chamada;', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalled();
+  })
 });
