b9d9547d7d521ba21e3ccaa3c425f88f7c02fab6
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 18:11:46 -0300

    Corrigindo Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -3,12 +3,13 @@ const fetchEndPoint = (product) => fetch(
 );
 
 const fetchProducts = async (product) => {
-  const endPoint = await fetchEndPoint(product);
-  if (!product) {
-    return Promise.reject(new Error('You must provide an url'));
+  try {
+    const endPoint = await fetchEndPoint(product);
+    const dataAPI = await endPoint.json();
+    return await dataAPI;
+  } catch (error) {
+    return 'You must provide an url';
   }
-  const dataAPI = await endPoint.json();
-  return dataAPI;
 };
 
 if (typeof module !== 'undefined') {
