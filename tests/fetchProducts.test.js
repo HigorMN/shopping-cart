cabeb6f6f8995cb790c2b2aaee7b46bf6aafba02
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 15:28:14 -0300

    testar se o fetch tem o mesmo link que foi chamdo

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,19 +4,19 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   test('Teste se fetchProducts é uma função;', () => {
-    expect(typeof fetchProducts).toBe('function')
+    expect(typeof fetchProducts).toBe('function');
   })
 
   test('Execute a função fetchProducts com o argumento \'computador\' e teste se fetch foi chamada;', async () => {
     expect.assertions(1);
-    await fetchProducts('computador')
+    await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
   })
 
   test('Teste se, ao chamar a função fetchProducts com o argumento \'computador\', a função fetch utiliza o endpoint', async () => {
     expect.assertions(1);
-    await fetchProducts('computador')
+    await fetchProducts('computador');
     const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
-    expect(fetch).toBeCalledWith(endpoint)
+    expect(fetch).toBeCalledWith(endpoint);
   })
 });
