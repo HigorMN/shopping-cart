7620b24acfa6af35db20e2535b5711feba356dab
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-10 13:03:45 -0300

    Refatorando test 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -14,7 +14,7 @@ describe('3 - Teste a função saveCartItems', () => {
   test('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems.', async () => {
     expect.assertions(1);
     await saveCartItems();
-    expect(localStorage.getItem('cartItems', 1)).toHaveBeenCalled();
+    expect(localStorage.setItem('cartItems', 1)).toHaveBeenCalled();
   })
 
 });
