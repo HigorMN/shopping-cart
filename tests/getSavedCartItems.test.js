b7b733efcef67093dc87f46958a76ab3da838ceb
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-10 13:11:58 -0300

    Primeiro teste do getSavedCartItems feito

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,9 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', async () => {
+    expect.assertions(1);
+    await getSavedCartItems()
+    expect(localStorage.getItem).toHaveBeenCalled();
+  })
 });
