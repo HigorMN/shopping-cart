0ec66da2b0d9b22b4558dedabaedb5ff641b1107
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-09 21:15:58 -0300

    Requisito 6 concluido

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,9 +1,20 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
+const { price } = require('../mocks/item');
 
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;', async () => {
+    expect.assertions(1);
+    await saveCartItems();
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+
+  test('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems.', async () => {
+    expect.assertions(1);
+    await saveCartItems();
+    expect(localStorage.getItem('cartItems', 1)).toBeCalledWith();
+  })
+
 });
